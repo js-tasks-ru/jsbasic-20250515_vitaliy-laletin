3e7bbc5d384e34c28600f78633ff435dd1091bd4
LVJameskirk <LVJameskirk@gmail.com>
2025-05-24 18:03:59 +0300

    add: implementation for JS Objects

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,11 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+
+  for (let prop in salaries) {
+    if (typeof salaries[prop] !== "number") continue;
+
+    sum += isFinite(salaries[prop]) ? salaries[prop] : 0;
+  }
+
+  return sum;
 }
